5255b4fb89f26ddcd1f763b17d4e68ece9846727
tyler-evans0 <tje1857@gmail.com>
2025-04-18 22:31:55 -0700

    updating next.config.js file to better rely on page.tsx file for index

--- next.config.js
+++ next.config.js
@@ -1,20 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/kona_sales_app' : '',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'ext.same-assets.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/kona_sales_app/' : '',
   trailingSlash: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
