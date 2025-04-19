90150b41c390fbe385b625ec778664efe0d2083b
tyler-evans0 <tje1857@gmail.com>
2025-04-18 22:56:25 -0700

    layout.tsx update header/footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 import ClientBody from "./ClientBody";
 
 const poppins = Poppins({
