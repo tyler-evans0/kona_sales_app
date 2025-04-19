0a97805a23a73fc9b544b4f8ba6ac8a2c8594df1
tyler-evans0 <tje1857@gmail.com>
2025-04-18 18:39:04 -0700

    Added Next.js config for pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/kona_sales_app',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ext.same-assets.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // This is required for GitHub Pages
+  assetPrefix: '/kona_sales_app/',
+  trailingSlash: true,
+}
+
+module.exports = nextConfig/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
   distDir: 'out',
   images: {
     unoptimized: true,
